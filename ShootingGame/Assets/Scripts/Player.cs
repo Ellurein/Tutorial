f332641498e9e0ed45f0b87d6d94bb456b11d130
Ellurein <ellurein.yami@gmail.com>
2015-07-31 16:19:13 +0900

    UnityTutorial-02

---
+++ ShootingGame/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	//移動スピード
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//右・左
+		float x = Input.GetAxisRaw ("Horizontal");
+	
+		//上・下
+		float y = Input.GetAxisRaw ("Vertical");
+
+		//移動する向き決定
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		//移動する向きとスピード代入
+		GetComponent<Rigidbody2D>().velocity = direction * speed;
+	}
+}
