1ad3773ff8ddb4d4ffb2b1da3be29e64e9405576
Ellurein <ellurein.yami@gmail.com>
2015-08-01 14:24:04 +0900

    UnityTutorial-03

--- ShootingGame/Assets/Scripts/Player.cs
+++ ShootingGame/Assets/Scripts/Player.cs
@@ -6,11 +6,20 @@ public class Player : MonoBehaviour {
 	//移動スピード
 	public float speed = 5;
 
-	// Use this for initialization
-	void Start () {
-	
+	//PlayerBulletプレハブ
+	public GameObject bullet;
+
+	//Startメソッドをコルーチンとして呼び出す
+	IEnumerator Start()
+	{
+		while (true) {
+			//弾をプレイヤーと同じ位置/角度で作成
+			Instantiate (bullet, transform.position, transform.rotation);
+			//0.05秒待つ
+			yield return new WaitForSeconds (0.05f);
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		//右・左
